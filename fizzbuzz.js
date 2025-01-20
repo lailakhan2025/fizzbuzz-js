41371096865b2f12869ba6d4d8600a59bf610895
lailakhan2025 <lailatariqkhan@gmail.com>
2025-01-20 15:06:20 +0000

    Part 4

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,8 +8,16 @@ function fizzbuzz() {
 
     // Put your code here...
     for (let i=1; i<=100; i++){
-        if(i%3==0){
+        if(i%3==0&&i%5==0){
+            console.log("fizzbuzz");
+        }
+        else if (i%3==0){
             console.log("fizz");
+
+        }
+        else if (i%5==0){
+            console.log("buzz");
+
         }
         else{
         console.log(i);
